b017ef9b3a6eda97df12ae25f6f576238d49cfdb
Christian Vazquez <christian.vazquez@ticc.com.mx>
2023-03-26 14:10:27 -0600

    build fixed

--- src/domain/repositories/IProductRepo.ts
+++ src/domain/repositories/IProductRepo.ts
@@ -1,4 +1,4 @@
-import Product from "../entities/product";
+import Product from "../entities/Product";
 import Response from "../entities/Response";
 
 interface IProductRepo {
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import dotenv from "dotenv";
+import { resolve } from 'path';
 
 dotenv.config();
 
@@ -13,4 +14,9 @@ export default defineConfig({
       return env;
     }, {}),
   },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src")
+    }
+  }
 })
